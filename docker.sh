390d4e03553b24da04210d3dd6b251243610185d
cloud_prg <cloud_prg@163.com>
2024-04-06 21:10:40 +0800

    chore: 更改镜像构建平台&补充readme

--- docker.sh
+++ docker.sh
@@ -1,12 +1,13 @@
 version=v0.0.1
 tag=cloudprg/blog:${version}
+platform=linux/amd64
 
 echo "开始打包${tag}"
 pnpm run build
 echo "结束打包"
 
 echo "开始构建镜像${tag}"
-docker build -t ${tag} .
+docker build --platform ${platform} -t ${tag} .
 echo "镜像构建完成"
 
 docker push ${tag}
