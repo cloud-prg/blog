067a7ef054eb6531417cfd61453670e09f958c94
cloud_prg <cloud_prg@163.com>
2024-04-19 10:23:24 +0800

    chore: 元数据更新

--- src/constant/seo.ts
+++ src/constant/seo.ts
@@ -1,6 +1,6 @@
 export const defaultSeoConfig = {
-    title: "云程的个人网站(前端技术)",
-    description: "主要分享前端技术、开源项目、生活感悟。",
+    title: "云程博客(Cloudprg Blog)",
+    description: "分享前端技术、开发经验、生活感悟。",
     // openGraph: {
     //     type: 'website',
     //     locale: 'en_IE',
@@ -15,6 +15,6 @@ export const defaultSeoConfig = {
 }
 
 export const homeSeoConfig = {
-    title: "Cloudprg(云程)技术博客",
-    description: "技术分享、开源生活，主要钻研在前端领域。",
+    title: "云程博客(Cloudprg Blog)",
+    description: "分享前端技术、开发经验、生活感悟。",
 }
