47838a8a7dbbd0d33dd6e25b2db9089cc67497f7
cloud_prg <cloud_prg@163.com>
2024-05-08 21:53:16 +0800

    chore: update version

--- setupProxy.js
+++ setupProxy.js
@@ -3,10 +3,11 @@
 const env = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
 
 const serverMap = {
-  // dev: 'http://localhost:3002/api',
   dev: 'http://114.55.7.80:4002/api',
+  // dev: 'http://114.55.7.80:4002/api',
   // prod: 'http://localhost:3002/api',
-  prod: 'http://114.55.7.80:4002/api',
+  // prod: 'https://114.55.7.80:4002/api',
+  prod: 'https://api.cloudprg.cn:4002/api',
 }
 
 const proxySuffix = serverMap[env];
