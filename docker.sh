72283e6e5517b35e3b497c0d4269ece2fdfe2668
cloud_prg <cloud_prg@163.com>
2024-05-09 10:08:41 +0800

    chore: update version

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ pnpm run build
 echo "结束打包"
 
 echo "开始构建镜像${tag}"
-docker build --platform ${platform} -t ${tag} .
+sudo docker build --platform ${platform} -t ${tag} .
 echo "镜像构建完成"
 
-docker push ${tag}
+sudo docker push ${tag}
